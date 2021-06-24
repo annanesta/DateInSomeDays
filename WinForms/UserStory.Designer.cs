048be98274f697b71f60367ab0d3acbd08be2592
Anna <Anna@ANNANESTER>
2021-06-25 00:21:36 +0300

    -добавлено реверсивное представление истории (в обратном порядке)

---
+++ WinForms/UserStory.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace DateInSomeDays
+{
+    partial class UserStory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.BtnReverce = new System.Windows.Forms.Button();
+            this.BtnDeleteItem = new System.Windows.Forms.Button();
+            this.BtnDeleteAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 52);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(384, 448);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // BtnReverce
+            // 
+            this.BtnReverce.AutoSize = true;
+            this.BtnReverce.Location = new System.Drawing.Point(13, 13);
+            this.BtnReverce.Name = "BtnReverce";
+            this.BtnReverce.Size = new System.Drawing.Size(85, 23);
+            this.BtnReverce.TabIndex = 1;
+            this.BtnReverce.Text = "Reverce order";
+            this.BtnReverce.UseVisualStyleBackColor = true;
+            this.BtnReverce.Click += new System.EventHandler(this.BtnReverce_Click);
+            // 
+            // BtnDeleteItem
+            // 
+            this.BtnDeleteItem.AutoSize = true;
+            this.BtnDeleteItem.Location = new System.Drawing.Point(104, 13);
+            this.BtnDeleteItem.Name = "BtnDeleteItem";
+            this.BtnDeleteItem.Size = new System.Drawing.Size(85, 23);
+            this.BtnDeleteItem.TabIndex = 1;
+            this.BtnDeleteItem.Text = "Delete an item";
+            this.BtnDeleteItem.UseVisualStyleBackColor = true;
+            // 
+            // BtnDeleteAll
+            // 
+            this.BtnDeleteAll.AutoSize = true;
+            this.BtnDeleteAll.Location = new System.Drawing.Point(195, 13);
+            this.BtnDeleteAll.Name = "BtnDeleteAll";
+            this.BtnDeleteAll.Size = new System.Drawing.Size(85, 23);
+            this.BtnDeleteAll.TabIndex = 1;
+            this.BtnDeleteAll.Text = "Delete all";
+            this.BtnDeleteAll.UseVisualStyleBackColor = true;
+            // 
+            // UserStory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 512);
+            this.Controls.Add(this.BtnDeleteAll);
+            this.Controls.Add(this.BtnDeleteItem);
+            this.Controls.Add(this.BtnReverce);
+            this.Controls.Add(this.listView1);
+            this.Name = "UserStory";
+            this.Text = "UserStory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button BtnReverce;
+        private System.Windows.Forms.Button BtnDeleteItem;
+        private System.Windows.Forms.Button BtnDeleteAll;
+    }
+}
---
+++ WinForms/UserStory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateInSomeDays
+{
+    public partial class UserStory : Form
+    {
+        public UserStory(Stack<StoryStruct>answer)
+        {
+            
+            InitializeComponent();
+            // Set to details view.
+            listView1.View = View.Details;
+            // Add a column with width 30 and left alignment.
+            listView1.Columns.Add("Operation", (listView1.Width/2)+10, HorizontalAlignment.Left);
+            listView1.Columns.Add("Date of the operation", (listView1.Width / 2) - 10, HorizontalAlignment.Left);
+            listView1.Scrollable = true;
+            foreach (StoryStruct i in answer)
+            {
+                listView1.Items.Add(new ListViewItem(new string[] { i.GetOperation, i.GetTime }));
+            }
+        }
+
+        //изменить порядок в обратную сторону
+        private void BtnReverce_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < (listView1.Items.Count / 2); i++)
+            {
+
+                string box10 = listView1.Items[i].SubItems[0].Text.ToString();
+                string box11 = listView1.Items[i].SubItems[1].Text.ToString();
+                string boxn0 = listView1.Items[(listView1.Items.Count - 1) - i].SubItems[0].Text.ToString();
+                string boxn1 = listView1.Items[(listView1.Items.Count - 1) - i].SubItems[1].Text.ToString();
+
+                listView1.Items[i] = (new ListViewItem(new string[] { boxn0.ToString(), boxn1.ToString() }));
+                listView1.Items[(listView1.Items.Count - 1) - i] = (new ListViewItem(new string[] { box10.ToString(), box11.ToString() }));
+            }
+        
+
+        }
+    }
+}
